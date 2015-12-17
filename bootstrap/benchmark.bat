9a6c77469d08012cc407ff88fbc005f541e0059c
Маздайщик <a.v.konovalov87@mail.ru>
2015-12-17 16:48:19 +0300

    mmx

--- bootstrap/benchmark.bat
+++ bootstrap/benchmark.bat
@@ -3,11 +3,11 @@ setlocal
   del int-%1 comp-%1
   set FILES=srefc Algorithm Context Error FindFile Generator Lexer ParseCmdLine Parser^
     SymTable refalrts Library LibraryEx
-  for /L %%i in (1, 1, 9) do (
+  for /L %%i in (1, 1, 3) do (
     echo Interpret %%i
     ..\compiler\srefc -c "%CPPLINE% -I../srlib -DINTERPRET" %FILES% 2>> int-%1
   )
-  for /L %%i in (1, 1, 9) do (
+  for /L %%i in (1, 1, 3) do (
     echo Compile %%i
     ..\compiler\srefc -c "%CPPLINE% -I../srlib" %FILES% 2>> comp-%1
   )
--- compiler/benchmark.bat
+++ compiler/benchmark.bat
@@ -1,5 +1,5 @@
 del %1
-for /L %%i in (1, 1, 9) do echo %%i & ..\compiler\srefc @benchmark_data.lst 2>> %1
+for /L %%i in (1, 1, 5) do echo %%i & ..\compiler\srefc @benchmark_data.lst 2>> %1
 del *.cpp
 echo.>> %1
 echo.>> %1
